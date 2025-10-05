91eb98345a371e6502dd74df25651679e48f0c9a
MeylishDLC <meylishy20@gmail.com>
2025-10-06 00:19:03 +0300

    feat: basic sound manager + volume slider

--- Assets/_Source/Installers/BootstrapInstaller.cs
+++ Assets/_Source/Installers/BootstrapInstaller.cs
@@ -1,17 +1,28 @@
 using Core;
+using SoundSystem;
+using UnityEngine;
 using Zenject;
 
 namespace Installers
 {
     public class BootstrapInstaller : MonoInstaller
     {
+        [SerializeField] private SoundManager soundManagerPrefab;
+
+        private SoundManager _soundManager;
         public override void InstallBindings()
         {
+            BindSoundManager();
             BindScoreSaver();
         }
         private void BindScoreSaver()
         {
             Container.Bind<ScoreSaver>().AsSingle();
+        }
+        private void BindSoundManager()
+        {
+            _soundManager = Container.InstantiatePrefabForComponent<SoundManager>(soundManagerPrefab);
+            Container.Bind<SoundManager>().FromInstance(_soundManager).AsSingle();
         }
     }
 }
---
+++ Assets/_Source/SoundSystem/FmodEventsConfig.cs
@@ -0,0 +1,13 @@
+﻿using FMODUnity;
+using UnityEngine;
+
+namespace SoundSystem
+{
+    [CreateAssetMenu(fileName = "FMOD Events Config", menuName = "Core/FMOD Events Config")]
+    public class FmodEventsConfig: ScriptableObject
+    {
+        [field: Header("----MUSIC----")] 
+        [field: SerializeField] public EventReference MenuMusic {get; private set;}
+        [field: SerializeField] public EventReference GameMusic {get; private set;}
+    }
+}
