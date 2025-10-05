abeb970b92d8963e9525e87a63b21246e4d9f63a
MeylishDLC <meylishy20@gmail.com>
2025-10-05 23:42:10 +0300

    feat: camera Y confiner + base background

---
+++ Assets/_Source/CameraLogic/ConfineDownOnly.cs
@@ -0,0 +1,30 @@
+﻿using Cinemachine;
+using UnityEngine;
+
+namespace CameraLogic
+{
+    [ExecuteAlways]
+    [SaveDuringPlay]
+    [AddComponentMenu("Cinemachine/Extensions/Confine Down Only")]
+    public class ConfineDownOnly : CinemachineExtension
+    {
+        [SerializeField] private float minY = -10f;
+
+        protected override void PostPipelineStageCallback(
+            CinemachineVirtualCameraBase vcam,
+            CinemachineCore.Stage stage,
+            ref CameraState state,
+            float deltaTime)
+        {
+            if (stage == CinemachineCore.Stage.Body)
+            {
+                var pos = state.RawPosition;
+                if (pos.y < minY)
+                {
+                    pos.y = minY;
+                }
+                state.RawPosition = pos;
+            }
+        }
+    }
+}
