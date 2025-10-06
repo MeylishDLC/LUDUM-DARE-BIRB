769a47a61bd4b9e416c24c530c5818bab594a767
MeylishDLC <meylishy20@gmail.com>
2025-10-06 17:12:55 +0300

    feat: tutorial window

--- Assets/_Source/Core/SceneController.cs
+++ Assets/_Source/Core/SceneController.cs
@@ -4,6 +4,9 @@ namespace Core
 {
     public class SceneController
     {
+        public int LastSceneIndex {get; private set;}
+        public int CurrentSceneIndex {get; private set;}
+
         private readonly int _mainSceneIndex;
         public SceneController(int mainSceneIndex)
         {
@@ -11,10 +14,21 @@ namespace Core
         }
         public void ReloadScene()
         {
+            LastSceneIndex = SceneManager.GetActiveScene().buildIndex; 
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        public void LoadScene(int sceneIndex)
+        {
+            LastSceneIndex = SceneManager.GetActiveScene().buildIndex; 
+            CurrentSceneIndex = sceneIndex;
+            
+            SceneManager.LoadScene(sceneIndex);
         }
         public void LoadGameScene()
         {
+            LastSceneIndex = SceneManager.GetActiveScene().buildIndex; 
+            CurrentSceneIndex = _mainSceneIndex;
+            
             SceneManager.LoadScene(_mainSceneIndex);
         }
     }
---
+++ Assets/_Source/Tutorial/TutorialWindow.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+using Core;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using InputSystem;
+using UnityEngine;
+using Zenject;
+
+namespace Tutorial
+{
+    public class TutorialWindow: MonoBehaviour
+    {
+        [SerializeField] private float animationDuration = 0.2f;
+        [SerializeField] private float scaleOnClose = 1.15f;
+        [SerializeField] Ease ease = Ease.Linear;
+        
+        private CancellationToken _ctOnDestroy;
+        private InputListener _listener;
+        private SceneController _sceneController;
+
+        [Inject]
+        public void Initialize(InputListener listener, SceneController sceneController)
+        {
+            _listener = listener;
+            _sceneController = sceneController;
+        }
+        private void Awake()
+        {
+            if (_sceneController.CurrentSceneIndex == _sceneController.LastSceneIndex)
+            {
+                gameObject.SetActive(false);
+                return;
+            }
+            _ctOnDestroy = this.GetCancellationTokenOnDestroy();
+            _listener.OnJumpStarted += CloseWindow;
+        }
+        
+        private void CloseWindow()
+        {
+            _listener.OnJumpStarted -= CloseWindow;
+            gameObject.transform.DOScale(new Vector3(scaleOnClose, scaleOnClose, scaleOnClose), animationDuration)
+                .SetEase(ease).ToUniTask(cancellationToken: _ctOnDestroy)
+                .ContinueWith(() => Destroy(gameObject)).Forget();
+        }
+    }
+}
